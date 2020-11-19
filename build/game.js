da2ff3a18b5ac15e61982b02342f92b6641232e7
Qubix <qubix27is3cubed@gmail.com>
2020-11-20 00:11:47 +0500

    removed trash from build

--- build/game.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const account_1 = require("./account");
-class Game {
-    constructor(user_id) {
-        this.account = new account_1.Account(user_id);
-    }
-}
-exports.Game = Game;
--- build/inf_deck.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const card_1 = require("./card");
-class InfDeck {
-    next() {
-        return card_1.Card.Random();
-    }
-}
-exports.InfDeck = InfDeck;
